c20dcdc495a11142a3124e0e2779c35711625ffb
Omkar Shirsekar <113287668+PRIME08012004@users.noreply.github.com>
2024-03-13 12:24:11 +0530

    added landing page

---
+++ Letusfixit/script2.js
@@ -0,0 +1,20 @@
+let typed = new Typed(".coder", {
+    strings: ["Let Us Fix It"],
+    typeSpeed: 200,
+    backSpeed: 50,
+    loop: true
+});
+let typed1= new Typed("#servicessplasher", {
+    strings: ["Electricain","Cleaning","Plumber","Painter","Mechanic",],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+});
+ const button1=document.getElementById("button1");
+ button1.addEventListener("click", function() {
+    window.location.href="#";
+ });
+ const button2=document.getElementById("button2");
+ button2.addEventListener("click", function() {
+    window.location.href="#";
+ });
